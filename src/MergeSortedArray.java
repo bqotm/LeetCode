adfb5f0abe3d67886a863b30bc573a011f313017
bothmane <baq.othmane@outlook.fr>
2021-10-01 00:27:57 +0100

    fix idx

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -4,11 +4,10 @@ public class MergeSortedArray {
 
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1, j=n-1, l=m+n-1;
-        while(i>=0 && j>=0){
-            nums1[l--]=(nums1[i]<=nums2[j]) ? nums2[i--] : nums1[j--];
-        }
-        while (j>=0)
+        int l=m+n-1, i=m-1, j=n-1;
+        while(i>=0 && j>=0)
+            nums1[l--]=(nums1[i] >= nums2[j]) ? nums1[i--] : nums2[j--];
+        while(j>=0)
             nums1[l--]=nums2[j--];
     }
 
