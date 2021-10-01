09d6a62a7e6c678abf796424b508245a5c039590
bothmane <baq.othmane@outlook.fr>
2021-10-01 16:26:39 +0100

    fix

--- src/TopEasyInterviewQuestions/RemovDuplicatesfromSortedArray.java
+++ src/TopEasyInterviewQuestions/RemovDuplicatesfromSortedArray.java
@@ -6,23 +6,19 @@ public class RemovDuplicatesfromSortedArray {
 
     public static int removeDuplicates(int[] nums) {
 
-        int i=0, j=0;
-        while(j<nums.length){
-            while(nums[j]==nums[i])
+        int j=0;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i]!=nums[j]){
+                nums[j]=nums[i];
                 j++;
-            for(int k=i+1; k<j; k++){
-                nums[k]=nums[j];
             }
-            i=i+1;
-            if(j==nums.length-1)
-                return i+1;
         }
-        return i+1;
+        return j;
     }
 
     public static void main(String[] args) {
         //int[] nums={0,0,1,1,1,2,2,3,3,4};
-        int[] nums=new int[]{1,1,2};
+        int[] nums={0,0,1,1,1,2,2,3,3,4};
         System.out.println(removeDuplicates(nums));
         System.out.println(Arrays.toString(nums));
     }
