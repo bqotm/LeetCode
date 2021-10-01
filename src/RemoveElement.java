c0401061b2a207edfb0834e16cdd4d860c9d61b9
bothmane <baq.othmane@outlook.fr>
2021-10-01 11:46:41 +0100

    inith

---
+++ src/RemoveElement.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class RemoveElement {
+
+
+
+    public static int removeElement(int[] nums, int val) {
+        int j=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+        return j;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr={0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(arr,2));
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
