6073783451f75ef73e08fb58c2ea68eb6ffeb8ef
bothmane <baq.othmane@outlook.fr>
2021-11-08 19:01:02 +0100

    AAC[]

---
+++ src/Permutations.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Permutations {
+
+
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        permutation(nums,0,res);
+        return res;
+
+    }
+
+    private void permutation(int[] nums, int start, List<List<Integer>> res) {
+        if(start==nums.length-1){
+            List<Integer> c= Arrays.stream(nums).boxed().collect(Collectors.toList());
+            res.add(c);
+        }
+
+        for(int i=start; i<nums.length; i++){
+            nums[start]=nums[start]^nums[i]^(nums[i]=nums[start]);
+            permutation(nums, start+1, res);
+            nums[start]=nums[start]^nums[i]^(nums[i]=nums[start]);
+        }
+    }
+
+
+}
