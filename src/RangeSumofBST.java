d4fe72332d9a4ada71e42f7501c1a2553f9dea3e
bothmane <baq.othmane@outlook.fr>
2021-11-12 15:26:26 +0100

    AAP[lil optmz]

--- src/RangeSumofBST.java
+++ src/RangeSumofBST.java
@@ -1,6 +1,6 @@
 public class RangeSumofBST {
 
-    int sum=0;
+   /* int sum=0;
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root!=null){
             rangeSumBST(root.left, low, high);
@@ -8,6 +8,16 @@ public class RangeSumofBST {
             rangeSumBST(root.right, low, high);
         }
         return sum;
+    }*/
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root==null)
+            return 0;
+        if(root.val<low)
+            return rangeSumBST(root.right, low, high);
+        if(root.val>high)
+            return rangeSumBST(root.left, low, high);
+        return root.val+rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
     }
 
 }
