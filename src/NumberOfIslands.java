51b03b6bfa74f8d78439f700bd49575143683387
othmane <baq.othmane@outlook.fr>
2022-07-09 22:46:12 +0100

    been a while

--- src/NumberOfIslands.java
+++ src/NumberOfIslands.java
@@ -1,2 +1,4 @@
-package PACKAGE_NAME;public class NumberOfIslands {
+public class NumberOfIslands {
+
+
 }
--- src/_Floodfill.java
+++ src/_Floodfill.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class _Floodfill {
+import java.util.Arrays;
+
+public class _Floodfill {
+
+
+
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int oldColor=image[sr][sc];
+        if(color!=image[sr][sc])
+            changeColor(image,sr,sc,color,oldColor);
+        return image;
+    }
+
+
+    public static void changeColor(int[][] image, int r, int c, int color, int oldColor){
+
+        if(image[r][c]==oldColor) {
+            image[r][c] = color;
+            if(r>=1)
+                changeColor(image, r - 1, c, color, oldColor);
+            if(c>=1)
+                changeColor(image, r, c - 1, color, oldColor);
+            if(r<image.length-1)
+                changeColor(image, r + 1, c, color, oldColor);
+            if(c<image[0].length-1)
+                changeColor(image, r, c + 1, color, oldColor);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] image={{1,1,1},{1,1,0},{1,0,1}};
+        System.out.println(Arrays.toString(floodFill(image,1,1,2)));
+    }
+
+
+
+
+
+
 }
